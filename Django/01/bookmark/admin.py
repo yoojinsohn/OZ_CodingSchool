9a8b8b2183f52015f1add5e4b5fe979353632404
Yoojin Sohn <ysohn06@gmail.com>
2026-03-31 13:41:08 +0900

    ✨feat: chapter_01 과제 업로드

---
+++ Django/01/bookmark/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from bookmark.models import Bookmark
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ['name', 'url']
+    list_display_links = ['name', 'url']
+    list_filter = ['name', 'url']
+
+#admin.site.register(Bookmark)
+
